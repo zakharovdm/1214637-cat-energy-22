949a4abf65d097a649105b4ab075fd463f527435
Dmitry Zakharov <dzaharov794@gmail.com>
2021-04-21 08:46:28 +0300

    Добавляет декоративные элементы в шапку на главной странице

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+let siteNav = document.querySelector('.site-nav');
+let navToggle = document.querySelector('.site-nav__toggle');
+
+siteNav.classList.remove('site-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (siteNav.classList.contains('site-nav--closed')) {
+    siteNav.classList.remove('site-nav--closed');
+    siteNav.classList.add('site-nav--opened');
+  } else {
+      siteNav.classList.add('site-nav--closed');
+      siteNav.classList.remove('site-nav--opened');
+  }
+});
