299ebdefa9f779915f17caa02e242b447963c88b
Dmitry Zakharov <dzaharov794@gmail.com>
2021-05-14 10:19:29 +0300

    Заменяет одинарные кавычки на двойные

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
-let siteNav = document.querySelector('.site-nav');
-let navToggle = document.querySelector('.site-nav__toggle');
+let siteNav = document.querySelector(".site-nav");
+let navToggle = document.querySelector(".site-nav__toggle");
 
-siteNav.classList.remove('site-nav--nojs');
+siteNav.classList.remove("site-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (siteNav.classList.contains('site-nav--closed')) {
-    siteNav.classList.remove('site-nav--closed');
-    siteNav.classList.add('site-nav--opened');
+navToggle.addEventListener("click", function() {
+  if (siteNav.classList.contains("site-nav--closed")) {
+    siteNav.classList.remove("site-nav--closed");
+    siteNav.classList.add("site-nav--opened");
   } else {
-      siteNav.classList.add('site-nav--closed');
-      siteNav.classList.remove('site-nav--opened');
+      siteNav.classList.add("site-nav--closed");
+      siteNav.classList.remove("site-nav--opened");
   }
 });
